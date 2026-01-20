4d85fc8691e2fbd071f47578168c0336d6f0c240
FritzLang <gao-zihan@qq.com>
2026-01-20 15:22:16 +0800

    I can't figure out what I'm doing

--- DeepLearning_Project/tool.py
+++ DeepLearning_Project/tool.py
@@ -5,6 +5,59 @@ from torchvision import transforms
 from matplotlib import pyplot as plt
 from IPython import display
 
+class Draw:
+    def __init__(self):
+        pass
+
+    def use_svg_display(self):
+        display.set_matplotlib_formats('svg')
+
+    def set_figsize(self, figsize=(3.5, 2.5)):
+        self.use_svg_display()
+        plt.rcParams['figure.figsize'] = figsize
+
+    def set_axes(self, axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+        axes.set_xlabel(xlabel)
+        axes.set_ylabel(ylabel)
+        axes.set_xscale(xscale)
+        axes.set_yscale(yscale)
+        axes.set_xlim(xlim)
+        axes.set_ylim(ylim)
+        if legend:
+            axes.legend(legend)
+        axes.grid()
+
+    def plot(self, X, Y=None, xlabel=None, ylabel=None, legend=None,xlim=None,
+         ylim=None, xscale='linear', yscale='linear',
+         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(3.5, 2.5),axes=None):
+        if legend is None:
+            legend = []
+
+        self.set_figsize(figsize)
+        axes = axes if axes else plt.gca()
+        def has_one_axis(X):
+            return (hasattr(X, "ndim") and X.ndim == 1 or
+                isinstance(X, list) and len(X) > 0 and
+                not hasattr(X[0], "__len__"))
+        if has_one_axis(X):
+            X = [X]
+        if Y is None:
+            X, Y = [[]] * len(X), X
+        elif has_one_axis(Y):
+            Y = [Y]
+        if len(X) != len(Y):
+            X = X * len(Y)
+        axes.cla()
+        for x, y, fmt in zip(X, Y, fmts):
+            if len(x):
+                axes.plot(x, y, fmt)
+            else:
+                axes.plot(y, fmt)
+        self.set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+    def scatter(self, x, y, s=1):
+        plt.scatter(x, y, s=s)
+
 def get_dataloader_workers():
     return 4
 
@@ -67,54 +120,6 @@ def train_epoch_ch3(net, train_iter, loss, updater):
         metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
     return metric[0] / metric[2], metric[1] / metric[2]
 
-def use_svg_display():
-    display.set_matplotlib_formats('svg')
-
-def set_figsize(figsize=(3.5, 2.5)):
-    use_svg_display()
-    plt.rcParams['figure.figsize'] = figsize
-
-def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
-    axes.set_xlabel(xlabel)
-    axes.set_ylabel(ylabel)
-    axes.set_xscale(xscale)
-    axes.set_yscale(yscale)
-    axes.set_xlim(xlim)
-    axes.set_ylim(ylim)
-    if legend:
-        axes.legend(legend)
-    axes.grid()
-
-def plot(X, Y=None, xlabel=None, ylabel=None, legend=None,xlim=None,
-         ylim=None, xscale='linear', yscale='linear',
-         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(3.5, 2.5),axes=None):
-    if legend is None:
-        legend = []
-
-    set_figsize(figsize)
-    axes = axes if axes else plt.gca()
-
-    def has_one_axis(X):
-        return (hasattr(X, "ndim") and X.ndim == 1 or
-                isinstance(X, list) and len(X) > 0 and
-                not hasattr(X[0], "__len__"))
-    if has_one_axis(X):
-        X = [X]
-    if Y is None:
-        X, Y = [[]] * len(X), X
-    elif has_one_axis(Y):
-        Y = [Y]
-    if len(X) != len(Y):
-        X = X * len(Y)
-    axes.cla()
-    for x, y, fmt in zip(X, Y, fmts):
-        if len(x):
-            axes.plot(x, y, fmt)
-        else:
-            axes.plot(y, fmt)
-    set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
-    
-
 class Animator:
     def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
                  ylim=None, xscale='linear', yscale='linear',
@@ -122,11 +127,11 @@ class Animator:
                  figsize=(3.5, 2.5)):
         if legend is None:
             legend = []
-        use_svg_display()
+        Draw.use_svg_display()
         self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
         if nrows * ncols == 1:
             self.axes = [self.axes, ]
-        self.config_axes = lambda: set_axes(
+        self.config_axes = lambda: Draw.set_axes(
             self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
         self.X, self.Y, self.fmts = None, None, fmts
 
@@ -150,3 +155,46 @@ class Animator:
         self.config_axes()
         display.display(self.fig)
         display.clear_output(wait=True)
+
+import time
+import math
+import numpy as np
+
+class Timer:
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        self.tik = time.time()
+
+    def stop(self):
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+    
+    def avg(self):
+        return sum(self.times) / len(self.times)
+    
+    def sum(self):
+        return sum(self.times)
+    
+    def cumsum(self):
+        return np.array(self.times).cumsum().tolist()
+    
+def synthetic_data(w, b, num_examples):
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+def linreg(X, w, b):
+    return torch.matmul(X, w) + b
+def squared_loss(y_hat, y):
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+def sgd(params, lr, batch_size):
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
