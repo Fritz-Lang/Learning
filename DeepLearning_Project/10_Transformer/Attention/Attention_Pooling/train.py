7698bdaa3f4ed0820f4c18354f13b8750025f3dc
Fritz <gao-zihan@qq.com>
2026-08-28 22:33:51 +0800

    修复一些bug

--- DeepLearning_Project/10_Transformer/Attention/Attention_Pooling/train.py
+++ DeepLearning_Project/10_Transformer/Attention/Attention_Pooling/train.py
@@ -23,31 +23,33 @@ y_hat = fn3(Config.x_train, Config.y_train, Config.n_test, Config.x_test)
 plot_kernel_reg(y_hat)
 plt.show()
 
-# '''带参数的注意力汇聚'''
-# # X_tile的形状:(n_train，n_train)，每一行都包含着相同的训练输入
-# X_tile = Config.x_train.repeat((Config.n_train, 1))
-# # Y_tile的形状:(n_train，n_train)，每一行都包含着相同的训练输出
-# Y_tile = Config.y_train.repeat((Config.n_train, 1))
-# # keys的形状:('n_train'，'n_train'-1)
-# keys = X_tile[(1 - torch.eye(Config.n_train)).type(torch.bool)].reshape((Config.n_train, -1))
-# # values的形状:('n_train'，'n_train'-1)
-# values = Y_tile[(1 - torch.eye(Config.n_train)).type(torch.bool)].reshape((Config.n_train, -1))
+'''带参数的注意力汇聚'''
+# X_tile的形状:(n_train，n_train)，每一行都包含着相同的训练输入
+X_tile = Config.x_train.repeat((Config.n_train, 1))
+# Y_tile的形状:(n_train，n_train)，每一行都包含着相同的训练输出
+Y_tile = Config.y_train.repeat((Config.n_train, 1))
+# keys的形状:('n_train'，'n_train'-1)
+keys = X_tile[(1 - torch.eye(Config.n_train)).type(torch.bool)
+              ].reshape((Config.n_train, -1))
+# values的形状:('n_train'，'n_train'-1)
+values = Y_tile[(1 - torch.eye(Config.n_train)).type(torch.bool)
+                ].reshape((Config.n_train, -1))
 
-# net = NWKernelRegression()
-# loss = nn.MSELoss(reduction='none')
-# trainer = torch.optim.SGD(net.parameters(), lr=0.5)
+net = NWKernelRegression()
+loss = nn.MSELoss(reduction='none')
+trainer = torch.optim.SGD(net.parameters(), lr=0.5)
 
-# for epoch in range(5):
-#     trainer.zero_grad()
-#     l = loss(net(Config.x_train, keys, values), Config.y_train)
-#     l.sum().backward()
-#     trainer.step()
-#     print(f'epoch {epoch + 1}, loss {float(l.sum()):.6f}')
+for epoch in range(5):
+    trainer.zero_grad()
+    l = loss(net(Config.x_train, keys, values), Config.y_train)
+    l.sum().backward()
+    trainer.step()
+    print(f'epoch {epoch + 1}, loss {float(l.sum()):.6f}')
 
-# # keys的形状:(n_test，n_train)，每一行包含着相同的训练输入（例如，相同的键）
-# keys = Config.x_train.repeat((Config.n_test, 1))
-# # value的形状:(n_test，n_train)
-# values = Config.y_train.repeat((Config.n_test, 1))
-# y_hat = net(Config.x_test, keys, values).unsqueeze(1).detach()
-# plot_kernel_reg(y_hat)
-# plt.show()
+# keys的形状:(n_test，n_train)，每一行包含着相同的训练输入（例如，相同的键）
+keys = Config.x_train.repeat((Config.n_test, 1))
+# value的形状:(n_test，n_train)
+values = Config.y_train.repeat((Config.n_test, 1))
+y_hat = net(Config.x_test, keys, values).unsqueeze(1).detach()
+plot_kernel_reg(y_hat)
+plt.show()
--- DeepLearning_Project/10_Transformer/Encoder-Decoder/src/Seq2SeqModel.py
+++ DeepLearning_Project/10_Transformer/Encoder-Decoder/src/Seq2SeqModel.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+
 class Seq2SeqEncoder(nn.Module):
     def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0.0):
         super().__init__()
@@ -10,19 +11,21 @@ class Seq2SeqEncoder(nn.Module):
     def forward(self, X):
         X = self.embedding(X)
         X = X.permute(1, 0, 2)
-        output,state = self.gru(X)
+        output, state = self.gru(X)
         return output, state
-    
+
+
 class Seq2SeqDecoder(nn.Module):
     def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0.0):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embed_size)
-        self.gru = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers, dropout=dropout)
+        self.gru = nn.GRU(embed_size + num_hiddens,
+                          num_hiddens, num_layers, dropout=dropout)
         self.dense = nn.Linear(num_hiddens, vocab_size)
 
     def init_state(self, encoder_output):
         return encoder_output[1]
-    
+
     def forward(self, X, H):
         X = self.embedding(X).permute(1, 0, 2)
         context = H[-1].repeat(X.shape[0], 1, 1)
@@ -30,7 +33,8 @@ class Seq2SeqDecoder(nn.Module):
         output, state = self.gru(X_and_context, H)
         output = self.dense(output).permute(1, 0, 2)
         return output, state
-    
+
+
 class EncoderDecoder(nn.Module):
     def __init__(self, encoder, decoder):
         super().__init__()
