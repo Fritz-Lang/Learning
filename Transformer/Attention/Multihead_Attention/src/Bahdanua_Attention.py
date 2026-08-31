830a80450c57d168f6c00ec5b93e74bfb23480d0
Fritz <gao-zihan@qq.com>
2026-08-31 16:09:50 +0800

    修复bug

---
+++ Transformer/Attention/Multihead_Attention/src/Bahdanua_Attention.py
@@ -0,0 +1,136 @@
+import torch
+from torch import nn
+
+
+class Seq2SeqEncoder(nn.Module):
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0.0):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.gru = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
+
+    def forward(self, X):
+        X = self.embedding(X)
+        X = X.permute(1, 0, 2)
+        output, state = self.gru(X)
+        return output, state
+
+
+"""
+基于加性注意力的Bahdanua注意力
+"""
+
+# 无注意力的解码器，只是将编码器的最终隐状态作为上下文向量
+# 然后将上下文向量和每一步输入拼接作为解码器的输入
+# 但，实际上最终隐状态中有很多无需关注的信息
+# 解码器的每一步输入只需要上下文向量中对应的信息即可
+# 但上下文向量没法拆分
+# 可以利用注意力机制，将源序列中我想要关注的某一步的信息重点注意
+# 生成新的对应步的上下文向量
+# 而且每一步上下文向量是由前一步的隐状态注意到的
+# 所以，每一步的上下文向量是循环生成的
+# 生成下一步的隐状态时，输入仍然是上下文向量与目标序列的输入拼接，然后和当前步的隐状态计算得出
+
+
+def sequence_mask(X, valid_len, value=0.0):
+    """在序列中屏蔽不相关的项"""
+    maxlen = X.size(1)
+    mask = (
+        torch.arange((maxlen), dtype=torch.float32, device=X.device)[None, :]
+        < valid_len[:, None]
+    )
+    X[~mask] = value
+    return X
+
+
+def masked_softmax(X, valid_lens):
+    """通过在最后一个轴上掩蔽元素来执行softmax操作"""
+    # X:3D张量，valid_lens:1D或2D张量
+    if valid_lens is None:
+        return nn.functional.softmax(X, dim=-1)
+    else:
+        shape = X.shape
+    if valid_lens.dim() == 1:
+        valid_lens = torch.repeat_interleave(valid_lens, shape[1])
+    else:
+        valid_lens = valid_lens.reshape(-1)
+    # 最后一轴上被掩蔽的元素使用一个非常大的负值替换，从而其softmax输出为0
+    X = sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=-1e6)
+    return nn.functional.softmax(X.reshape(shape), dim=-1)
+
+
+class AdditiveAttention(nn.Module):
+    def __init__(self, key_size, query_size, num_hiddens, dropout):
+        super().__init__()
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=False)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=False)
+        self.w_v = nn.Linear(num_hiddens, 1, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, queries, keys, values, valid_lens):
+        queries, keys = self.W_q(queries), self.W_k(keys)
+        features = queries.unsqueeze(2) + keys.unsqueeze(1)
+        features = torch.tanh(features)
+        scores = self.w_v(features).squeeze(-1)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        return torch.bmm(self.dropout(self.attention_weights), values)
+
+
+class Seq2SeqAttentionDecoder(nn.Module):
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0.0):
+        super().__init__()
+        self.attention = AdditiveAttention(
+            num_hiddens, num_hiddens, num_hiddens, dropout
+        )
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.gru = nn.GRU(
+            embed_size + num_hiddens, num_hiddens, num_layers, dropout=dropout
+        )
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, encoder_outputs, encoder_valid_lens):
+        outputs, hidden_state = encoder_outputs
+        return (outputs.permute(1, 0, 2), hidden_state, encoder_valid_lens)
+
+    def forward(self, X, H):
+        # enc_outputs的形状为(batch_size,num_steps,num_hiddens).
+        # hidden_state的形状为(num_layers,batch_size, num_hiddens)
+        encoder_outputs, hidden_state, encoder_valid_lens = H
+        X = self.embedding(X).permute(1, 0, 2)
+        outputs, self._attention_weights = [], []
+        for x in X:
+            # query的形状为(batch_size,1,num_hiddens)
+            query = torch.unsqueeze(hidden_state[-1], dim=1)
+            # context的形状为(batch_size,1,num_hiddens)
+            context = self.attention(
+                query, encoder_outputs, encoder_outputs, encoder_valid_lens
+            )
+            # 在特征维度上连结
+            x = torch.cat((context, torch.unsqueeze(x, dim=1)), dim=-1)
+            # 将x变形为(1,batch_size,embed_size+num_hiddens)
+            out, hidden_state = self.gru(x.permute(1, 0, 2), hidden_state)
+            outputs.append(out)
+            self._attention_weights.append(self.attention.attention_weights)
+        # 全连接层变换后，outputs的形状为
+        # (num_steps,batch_size,vocab_size)
+        outputs = self.dense(torch.cat(outputs, dim=0))
+        return outputs.permute(1, 0, 2), [
+            encoder_outputs,
+            hidden_state,
+            encoder_valid_lens,
+        ]
+
+    @property
+    def attention_weights(self):
+        return self._attention_weights
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, encoder_X, decoder_X, encoder_valid_lens=None):
+        encoder_output = self.encoder(encoder_X)
+        dec_state = self.decoder.init_state(encoder_output, encoder_valid_lens)
+        return self.decoder(decoder_X, dec_state)
---
+++ Transformer/Attention/Multihead_Attention/src/data_loader.py
@@ -0,0 +1,173 @@
+import os
+import pickle
+import sys
+from collections import Counter
+
+import torch
+from Config import Config
+from torch.utils.data import DataLoader, Dataset
+
+DATA_FILE = os.path.join(Config.PROJECT_ROOT, "data")
+TXT_FILE = os.path.join(DATA_FILE, "cmn-eng.txt")
+ENGLISH_VOCAB_FILE = os.path.join(DATA_FILE, "ENGLISH_VOCAB_FILE")
+CHINESE_VOCAB_FILE = os.path.join(DATA_FILE, "CHINESE_VOCAB_FILE")
+
+
+def check_data_exists():
+    """检查数据文件是否存在"""
+    if not os.path.exists(TXT_FILE):
+        print("未找到文件")
+        sys.exit(1)
+
+
+def read_translation_data(path):
+    """数据清洗，列表包含对应字符串"""
+    src_data = []
+    tgt_data = []
+    with open(path, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.strip("\n").split("\t")
+            src_data.append(line[0])
+            tgt_data.append(line[1])
+    return src_data, tgt_data
+
+
+def tokenize(data):
+    """将字符串拆分成对应单词级列表"""
+    tokenize_data = []
+
+    punct = set(",.!?。？！，、")
+    for line in data:
+        tokens = []
+        i = 0
+        n = len(line)
+
+        while i < n:
+            ch = line[i]
+
+            # 1. 如果是英文字母，连续取完作为一个单词
+            if "a" <= ch <= "z" or "A" <= ch <= "Z":
+                word = ""
+                while i < n and (("a" <= line[i] <= "z") or ("A" <= line[i] <= "Z")):
+                    word += line[i]
+                    i += 1
+                tokens.append(word)
+                continue  # 跳过末尾的 i += 1
+
+            # 2. 如果是标点，单独作为一个词元
+            elif ch in punct:
+                tokens.append(ch)
+                i += 1
+                continue
+
+            # 3. 其他字符（包括中文、数字等），按单个字符拆分
+            else:
+                # 跳过空格（如果不想保留空格的话）
+                if not ch.isspace():
+                    tokens.append(ch)
+                i += 1
+
+        tokenize_data.append(tokens)
+
+    return tokenize_data
+
+
+def create_vocab(data, num_steps, vocab_path):
+    """根据文本列表构建对应词表"""
+    tokenized_data = tokenize(data)
+    listed_data = [token for line in tokenized_data for token in line]  # 二维列表展平
+
+    counter = Counter(listed_data)
+    tokens_freq = sorted(counter.items(), key=lambda x: x[1])
+    freq2_text = [word for word, freq in tokens_freq if freq > 1]
+
+    vocab = ["<pad>", "<unk>", "<bos>", "<eos>"] + freq2_text
+    vocab_size = len(vocab)
+
+    token_to_ix = {ch: i for i, ch in enumerate(vocab)}
+    ix_to_token = {i: ch for i, ch in enumerate(vocab)}
+
+    with open(vocab_path, "wb") as file:
+        pickle.dump((token_to_ix, ix_to_token, vocab_size), file)
+    print(f"词表存入{vocab_path}, 词汇量大小{vocab_size}")
+
+    unk_idx = token_to_ix["<unk>"]
+    # 将二维列表原文转为二维整数序列
+    text_as_int = [
+        [token_to_ix.get(ch, unk_idx) for ch in line] for line in tokenized_data
+    ]
+
+    bos_idx = token_to_ix["<bos>"]
+    eos_idx = token_to_ix["<eos>"]
+    pad_idx = token_to_ix["<pad>"]
+
+    # 超长序列只保留前 num_steps - 1 个词元，留一个位置给 <bos> 或 <eos>
+    begin_seqs = []
+    end_seqs = []
+
+    for line in text_as_int:
+        line = line[: num_steps - 1]
+
+        # <bos> 放在序列首部，<eos> 放在序列尾部
+        begin_seq = [bos_idx] + line
+        end_seq = line + [eos_idx]
+
+        # 右侧补齐 <pad> 到统一长度 num_steps
+        if len(begin_seq) < num_steps:
+            begin_seq += [pad_idx] * (num_steps - len(begin_seq))
+        if len(end_seq) < num_steps:
+            end_seq += [pad_idx] * (num_steps - len(end_seq))
+
+        begin_seqs.append(begin_seq)
+        end_seqs.append(end_seq)
+
+    return begin_seqs, end_seqs, vocab_size, pad_idx
+
+
+def prepare_data(sequence_length=8):
+    """加载、预处理数据并创建DataLoader"""
+    check_data_exists()
+    print(f"从{TXT_FILE}读取数据")
+    src_data, tgt_data = read_translation_data(TXT_FILE)
+
+    src_input_seqs, _, src_vocab_size, _ = create_vocab(
+        src_data, sequence_length, ENGLISH_VOCAB_FILE
+    )
+    tgt_input_seqs, tgt_target_seqs, tgt_vocab_size, tgt_pad_idx = create_vocab(
+        tgt_data, sequence_length, CHINESE_VOCAB_FILE
+    )
+
+    class TranslateDataset(Dataset):
+        def __init__(self, src_input_seqs, tgt_input_seqs, tgt_target_seqs):
+            self.src_input_seqs = src_input_seqs
+            self.tgt_input_seqs = tgt_input_seqs
+            self.tgt_target_seqs = tgt_target_seqs
+
+        def __len__(self):
+            return len(self.src_input_seqs)
+
+        def __getitem__(self, idx):
+            return (
+                torch.tensor(self.src_input_seqs[idx], dtype=torch.long),
+                torch.tensor(self.tgt_input_seqs[idx], dtype=torch.long),
+                torch.tensor(self.tgt_target_seqs[idx], dtype=torch.long),
+            )
+
+    dataset = TranslateDataset(src_input_seqs, tgt_input_seqs, tgt_target_seqs)
+
+    return dataset, src_vocab_size, tgt_vocab_size, tgt_pad_idx
+
+
+def get_data_loader(batch_size=64, sequence_length=8):
+    dataset, src_vocab_size, tgt_vocab_size, tgt_pad_idx = prepare_data(sequence_length)
+
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(
+        dataset, [train_size, test_size]
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader, src_vocab_size, tgt_vocab_size, tgt_pad_idx
